d16e995f702301b25dc6478914c20f5742d93b03
sergio <sergiher@gmail.com>
2025-07-10 15:58:55 +0200

    Bonus: BE: error text if no GOOGLE_API_KEY

--- backend/app/infrastructure/external_services/google/gemini.py
+++ backend/app/infrastructure/external_services/google/gemini.py
@@ -1,7 +1,5 @@
 import os
-from os.path import dirname, join
 
-from dotenv import load_dotenv
 from google import genai  # type: ignore
 
 
@@ -9,14 +7,21 @@ class GoogleService:
     def __init__(self, model_name="gemini-2.0-flash-001"):
         self.model_name = model_name
         self.api_key = os.getenv("GOOGLE_API_KEY")
-        self.client = genai.Client(api_key=self.api_key)
+        if not self.api_key:
+            self.client = None
+        else:
+            self.client = genai.Client(api_key=self.api_key)
 
-    def generate_response(self, input_text):
+    def generate_response(self, input_text: str):
+        if not self.client:
+            return "Please provide a GOOGLE_API_KEY"
         response = self.client.models.generate_content(
             model=self.model_name, contents=input_text
         )
         return response
 
-    def explain_about(self, input_text):
+    def explain_about(self, input_text: str) -> str:
         response = self.generate_response(input_text)
+        if isinstance(response, str):
+            return response
         return response.text
