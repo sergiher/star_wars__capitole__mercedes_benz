f68ae44055f77d8f92c0468608ab33b776cd7e10
sergio <sergiher@gmail.com>
2025-07-10 16:03:21 +0200

    BE, FE: default env values

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,8 +4,8 @@ import os
 class Settings:
     PROJECT_NAME: str = "Starwars app"
     PROJECT_VERSION: str = "1.0.0"
-    FRONTEND_WEB_BASE_URL = os.getenv("FRONTEND_WEB_BASE_URL")
-    ENTITIES_LIST = os.getenv("ENTITIES_LIST")
+    FRONTEND_WEB_BASE_URL = os.getenv("FRONTEND_WEB_BASE_URL", "http://localhost:6969")
+    ENTITIES_LIST = os.getenv("ENTITIES_LIST", {"people", "planets"})
 
 
 # In case I have to use some config only for tests:
--- frontend/app/infrastructure/api/dataApi.ts
+++ frontend/app/infrastructure/api/dataApi.ts
@@ -7,25 +7,22 @@ export async function postSortingDataApi(
   }[]
 ) {
   const hasSorting = sortParams && sortParams.length > 0;
-
-  const response = await fetch(
-    `${import.meta.env.VITE_API_BASE_URL}/sort/${entity_type}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      ...(hasSorting && {
-        body: JSON.stringify({
-          field: sortParams[0]?.field ?? "name",
-          direction: sortParams[0]?.direction ?? "asc",
-          ...(sortParams[0]?.algorithm && {
-            algorithm: sortParams[0].algorithm,
-          }),
+  const baseUrl = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8000";
+  const response = await fetch(`${baseUrl}/sort/${entity_type}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    ...(hasSorting && {
+      body: JSON.stringify({
+        field: sortParams[0]?.field ?? "name",
+        direction: sortParams[0]?.direction ?? "asc",
+        ...(sortParams[0]?.algorithm && {
+          algorithm: sortParams[0].algorithm,
         }),
       }),
-    }
-  );
+    }),
+  });
 
   if (!response.ok) {
     throw new Error(`Failed to sort data`);
